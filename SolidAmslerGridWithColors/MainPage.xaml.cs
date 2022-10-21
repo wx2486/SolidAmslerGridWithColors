fa385355dbc02cacb594079459e48abd3ece4fc0
Xiang WU <xiangw.ahu@gmail.com>
2022-10-21 18:04:09 +0800

    a

--- SolidAmslerGridWithColors/MainPage.xaml.cs
+++ SolidAmslerGridWithColors/MainPage.xaml.cs
@@ -1,10 +1,35 @@
-﻿namespace SolidAmslerGridWithColors;
+﻿using Microsoft.Maui.Layouts;
+
+namespace SolidAmslerGridWithColors;
 
 public partial class MainPage : ContentPage
 {
     public MainPage()
     {
         InitializeComponent();
+    }
+
+    private void ShowPrivacyPolicy(object sender, EventArgs e)
+    {
+        Browser.Default.OpenAsync("https://raw.githubusercontent.com/wx2486/SolidAmslerGridWithColors/main/PrivacyPolicy.txt", BrowserLaunchMode.SystemPreferred);
+    }
+
+    private void SetChildrenSize(object sender, EventArgs e)
+    {
+        AbsoluteLayout mainLayout = this.Content as AbsoluteLayout;
+        ImageButton infoButton = mainLayout.First(_ => (_ as Element).ClassId is "InfoButton") as ImageButton;
+
+        double longEdge = Math.Max(this.Bounds.Width, this.Bounds.Height);
+        double shortEdge = Math.Min(this.Bounds.Width, this.Bounds.Height);
+        double infoButtonSize = Math.Max(longEdge / 2.0, shortEdge) / 20.0;
+        Rect infoButtonBounds = new Rect(this.Bounds.Left, this.Bounds.Top, infoButtonSize, infoButtonSize);
+
+        mainLayout.SetLayoutFlags(infoButton, AbsoluteLayoutFlags.None);
+        mainLayout.SetLayoutBounds(infoButton, infoButtonBounds);
+
+        GraphicsView mainFrame = mainLayout.First(_ => (_ as Element).ClassId is "MainFrame") as GraphicsView;
+        mainLayout.SetLayoutFlags(mainFrame, AbsoluteLayoutFlags.None);
+        mainLayout.SetLayoutBounds(mainFrame, this.Bounds);
     }
 }
 
@@ -21,18 +46,5 @@ public class MainFrame : IDrawable
             for (int col = 0; col * size < rectF.Width; col++)
                 if (row % 2 == 0 && (row + col) % 2 == 0)
                     canvas.FillRectangle(col * size, row * size, size, size);
-
-#if DEBUG
-        System.Text.StringBuilder sb = new System.Text.StringBuilder();
-
-        sb.AppendLine($"Pixel width: {DeviceDisplay.Current.MainDisplayInfo.Width} / Pixel Height: {DeviceDisplay.Current.MainDisplayInfo.Height}");
-        sb.AppendLine($"Density: {DeviceDisplay.Current.MainDisplayInfo.Density}");
-        sb.AppendLine($"Orientation: {DeviceDisplay.Current.MainDisplayInfo.Orientation}");
-        sb.AppendLine($"Rotation: {DeviceDisplay.Current.MainDisplayInfo.Rotation}");
-        sb.AppendLine($"Refresh Rate: {DeviceDisplay.Current.MainDisplayInfo.RefreshRate}");
-
-        canvas.FontColor = Colors.LemonChiffon;
-        canvas.DrawString($"{sb.ToString()} {rectF.X} {rectF.Y} {rectF.Width} {rectF.Height}", rectF, HorizontalAlignment.Left, VerticalAlignment.Top);
-#endif
     }
 }
